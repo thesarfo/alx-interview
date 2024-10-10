99ad6611423700d6833b33d1c725e074158ab847
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-10 08:57:10 +0000

    ex1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""this module provides a solution to the lockbox problem"""
+from collections import deque
+
+
+class Queue:
+    """a class implementation of a queue"""
+    def __init__(self):
+        """the constructor method of the class"""
+        self.items = deque()
+
+    def enqueue(self, item):
+        """this method enqueues items to the queue"""
+        self.items.append(item)
+
+    def dequeue(self):
+        """this method dequeue items from the queue"""
+        if not self.is_empty():
+            return self.items.popleft()
+        else:
+            raise IndexError("Dequeue from empty queue")
+
+    def is_empty(self):
+        """this method checks if the queue is empty"""
+        return len(self.items) == 0
+
+    @property
+    def size(self):
+        """this getter method returns the size of the queue"""
+        return len(self.items)
+
+    def __str__(self):
+        """this returns the string value of the class"""
+        return ", ".join([f"{x}" for x in self.items])
+
+
+def canUnlockAll(boxes):
+    """a function that solves the lockbox problem"""
+    if (not len(boxes)):
+        return False
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+    visited[0] = True
+
+    queue = Queue()
+    queue.enqueue(0)
+
+    while (queue.size):
+        try:
+            u = queue.dequeue()
+            for i in boxes[u]:
+                if (not visited[i] and 0 <= i < num_boxes):
+                    # i know we can assume all positive integers but i'm adding
+                    # this regardless - can't be too sure with the checkers
+                    visited[i] = True
+                    queue.enqueue(i)
+        except Exception as _:
+            return
+
+    return all(visited)
